c65a21de581fe6da96831b7f8a25462cb147c39c
Abdul Moiz <moizhus123@gmail.com>
2022-10-08 22:50:00 +0500

    Added assets - copy top clipboard icon. Improved basic styles of DataGrid. Added code samples which will be used in future. Added Youtube sources.

--- KeyFunctions_WPF/MainWindow.xaml.cs
+++ KeyFunctions_WPF/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,17 +22,55 @@ namespace KeyFunctions_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        //public ObservableCollection<object> asdf;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // source links:
+            // https://www.youtube.com/watch?v=ayp3tHEkRc0 (sqlite c# windows app dapper)
+            // https://www.youtube.com/watch?v=dOZYOnFb56Q
+            // https://www.youtube.com/watch?v=YW3gq-zyzoE
+            // https://www.youtube.com/watch?v=CNVR-Rp54aY
+            // https://www.youtube.com/watch?v=_LFxV1thbkQ
+            // https://parallelcodes.com/wpf-datagrid-with-button/
+            // successful keywords on google:
+            // C# dotnet datagridview obersablecollection
         }
 
         private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
+            // DataGrid_ClipHistory.DataContext // this is the one to be used.
+
+            DataGrid_ClipHistory.Items.Add(new
+            {
+                DateTime = DateTime.Now.ToString()
+            });
         }
 
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        private void CheckBox_OnStateChanged(object sender, RoutedEventArgs e)
         {
+            var checkbox = (CheckBox)sender;
+            Console.WriteLine($"{checkbox.Name} {checkbox.IsChecked}");
+
+            //DataGrid_ClipHistory.Items = new ItemCollection();
+            DataGrid_ClipHistory.Items.Add(new
+            {
+                DateTime = DateTime.Now.ToString()
+            });
+        }
+
+        private void CopyButtonView_OnClick(object sender, RoutedEventArgs e)
+        {
+            dynamic dataContext = ((Button)e.Source).DataContext;
+
+            Console.WriteLine(dataContext.DateTime);
+        }
+
+        private void aPicture_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("MouseButtonEventArgs");
         }
     }
 }
